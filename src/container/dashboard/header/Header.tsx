bd6a90a6810cb9b7747ba4fe6253fa9bf75ccf9c
rutyak <rutikkhandekar12@gmail.com>
2026-02-19 01:42:38 +0530

    style: header padding change

--- src/container/dashboard/header/Header.tsx
+++ src/container/dashboard/header/Header.tsx
@@ -40,13 +40,13 @@ const Header = () => {
           direction="column"
           align="center"
           justify="center"
-          pt={{ base: 10, md: "55px" }}
+          pt={{ base: "60px", md: "70px" }}
           textAlign="center"
         >
           <Heading
             as="h1"
             className="hero-title"
-            fontSize={{ base: "4xl", sm: "6xl", lg: "8xl" }}
+            fontSize={{ base: "4xl", sm: "6xl", md: "7xl", lg: "8xl" }}
           >
             Food <span className="highlight">Bazaar</span>
           </Heading>
@@ -54,13 +54,17 @@ const Header = () => {
           <Text
             className="hero-subtitle"
             fontSize={{ base: "sm", md: "xl" }}
-            mb={{ base: 8, md: 10 }}
+            mb={{ base: 6, md: 8 }}
             maxW={{ base: 100, md: 200 }}
           >
             Discover the best flavors delivered straight to your doorstep.
           </Text>
 
-          <Box w="full" maxW="750px" className="search-outer-wrapper">
+          <Box
+            w="full"
+            maxW={{ base: "520px", md: "580px", lg: "750px" }}
+            className="search-outer-wrapper"
+          >
             <Search setSearch={setSearch} search={search} />
           </Box>
         </Flex>
